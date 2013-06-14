301df353798eceeeb572fbea06c99d1191885a70
Soner Ozenc <sonerozenc@gmail.com>
2013-06-14 12:24:10 +0100

    added bits to route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Qa::Application.routes.draw do
+
+	scope "api" do
+    resources :questions, except: [ :new, :edit ], defaults: { format: :json }
+    resources :answers, except: [ :new, :edit ], defaults: { format: :json }
+  end
+
   get "users" => "home#index"
   get "users/new" => "home#index"
   get "users/:id" => "home#index"
